45de7dfe018ac9e5bd34a36b92c2184fafeec9e4
Ruan Caetano <ruansouza_caetano@hotmail.com>
2022-09-25 12:24:52 -0300

    feat: add product entity

---
+++ src/entities/product.spec.ts
@@ -0,0 +1,63 @@
+import { Product } from "./product";
+
+describe("Product entity unit tests", () => {
+  describe("Creation", () => {
+    it("should throw an error when id is empty", () => {
+      expect(() =>
+        new Product("", "Product 1", 100)
+      ).toThrowError("Id is required");
+    });
+
+    it("should throw an error when name is empty", () => {
+      expect(() =>
+        new Product("1", "", 100)
+      ).toThrowError("Name is required");
+    });
+
+    it("should throw an error when price is less than zero", () => {
+      expect(() =>
+        new Product("1", "Product 1", -1)
+      ).toThrowError("Price must be greater than zero");
+    });
+
+    it("should create a new product", () => {
+      const product = new Product("1", "Product 1", 100);
+
+      expect(product).toBeDefined();
+    });
+  });
+
+  describe("Change name", () => {
+    it("should throw an error when name is empty", () => {
+      const product = new Product("1", "Product 1", 100);
+
+      expect(() => {
+        product.changeName("");
+      }).toThrowError("Name is required");
+    });
+
+    it("should update name", () => {
+      const product = new Product("1", "Product 1", 100);
+
+      product.changeName("Product 1 edited");
+      expect(product.name).toBe("Product 1 edited");
+    });
+  });
+
+  describe("Change price", () => {
+    it("should throw an error when price is less than zero", () => {
+      const product = new Product("1", "Product 1", 100);
+
+      expect(() => {
+        product.changePrice(-1);
+      }).toThrowError("Price must be greater than zero");
+    });
+
+    it("should change price", () => {
+      const product = new Product("1", "Product 1", 100);
+
+      product.changePrice(200);
+      expect(product.price).toBe(200);
+    });
+  });
+});
---
+++ src/entities/product.ts
@@ -0,0 +1,48 @@
+export class Product {
+  private _id: string;
+  private _name: string;
+  private _price: number;
+
+  constructor(id: string, name: string, price: number) {
+    this._id = id;
+    this._name = name;
+    this._price = price;
+
+    this.validate();
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get price(): number {
+    return this._price;
+  }
+
+  validate(): boolean {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+    if (this._price < 0) {
+      throw new Error("Price must be greater than zero");
+    }
+    return true;
+  }
+
+  changeName(name: string) {
+    if (!name) {
+      throw new Error("Name is required");
+    }
+    this._name = name;
+  }
+
+  changePrice(price: number) {
+    if (price < 0) {
+      throw new Error("Price must be greater than zero");
+    }
+    this._price = price;
+  }
+}
