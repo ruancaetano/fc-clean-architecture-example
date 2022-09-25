5c9d1433ccaa2da2e7b9095eb603b91e55f54050
Ruan Caetano <ruansouza_caetano@hotmail.com>
2022-09-25 12:25:27 -0300

    feat: add order domain service

---
+++ src/services/order.service.ts
@@ -0,0 +1,22 @@
+import { v4 as uuid} from 'uuid'
+
+import { Customer } from "../entities/customer";
+import { Order } from "../entities/order";
+import { OrderItem } from "../entities/order-item";
+
+export class OrderService {
+  static placeOrder(customer: Customer, items: OrderItem[]): Order {
+    if (!items.length) {
+      throw new Error("Order must have at least one item");
+    }
+
+    const order = new Order(uuid(), customer.id, items);
+    customer.addRewardPoints(order.calculateTotal() / 2);
+
+    return order;
+  }
+
+  static calculateAllOrdersTotal(orders: Order[]): number {
+    return orders.reduce((sum, order) => sum + order.calculateTotal(), 0);
+  }
+}
